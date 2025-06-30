28a2c47e1337592c983be7eab6b62600db77518a
whoareyou <krbavadiya11@gmail.com>
2025-06-30 22:13:29 +0530

    done the pre-peration

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,8 +1,14 @@
-import { app, BrowserWindow } from 'electron'
-import path from 'path'
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import { isDev } from "./util.js";
 
+app.on("ready", () => {
+  // this runs when the app starts and is ready :) so it will run the callBack function..
+  const mainWindow = new BrowserWindow({}); // this can be configured such as we can add width, height etc.
 
-app.on("ready", () => { // this runs when the app starts and is ready :) so it will run the callBack function..
-    const mainWindow = new BrowserWindow({}); // this can be configured such as we can add width, height etc.
-    mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"))
-})
+  if (isDev()) {
+    mainWindow.loadURL("http://localhost:5123");
+  } else {
+    mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+  }
+});
